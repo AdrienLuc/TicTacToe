fd7beb76bab6d8e3b756709a9eba79ab05983065
Adrien <adrien.lucquiaud@gmail.com>
2021-08-26 09:37:16 +0200

    Added version 5

--- TicTacToe.py
+++ TicTacToe.py
@@ -29,6 +29,7 @@ p=""
 usa=0
 fofo=1
 pipi=0
+sos=1
 while fofo==1:
     print("    1   2   3  \n  +---+---+---+\n1 |   |   |   |\n  +---+---+---+\n2 |   |   |   |\n  +---+---+---+\n3 |   |   |   |\n  +---+---+---+")
 
@@ -36,12 +37,12 @@ while fofo==1:
         if usa==1:
         
             print("    1   2   3  ")
-            print("  +-",ln1,ln2,ln3,"--+---+---+")
-            print("1 |",k,"| ",p,"| ",üü,"|")
             print("  +---+---+---+")
-            print("2 |",l,"| ",ü,"| ",ää,"|")
+            print("1 |",ln1,k,"| ",p,"| ",üü,"|")
             print("  +---+---+---+")
-            print("3 |",j,"| ",ä,"| ",öö,"|")
+            print("2 |",ln2,l,"| ",ü,"| ",ää,"|")
+            print("  +---+---+---+")
+            print("3 |",ln3,j,"| ",ä,"| ",öö,"|")
             print("  +---+---+---+")
         
         if player==1:
@@ -52,15 +53,7 @@ while fofo==1:
         print ("Spieler",player,"(",asa,")"" ist dran.")
         x= int(input("Position x:"))
         y= int(input("Position y:"))
-
-        if x>3 or y>3:
-            print("Error - Falsche Eigabe! (Wähle zwischen 1-3)!")
-            if player==1:
-                player+=1
-                form="O"
-            else:
-                player-=1
-                form="X"
+            
         
         if player==1:
             player+=1
@@ -87,6 +80,14 @@ while fofo==1:
             ää=form
         elif x==3 and y==1 and üü=="":
             üü=form
+        elif x>3 or y>3:
+            print(bcolors.WARNING +"Error - Falsche Eigabe! (Wähle zwischen 1-3)!"+bcolors.ENDC)
+            if player==1:
+                player+=1
+                form="O"
+            else:
+                player-=1
+                form="X"
         else:
             print(bcolors.WARNING + "\tUnmöglicher Spielzug! Feld bereits besetzt!"+bcolors.ENDC)
             if player==1:
@@ -108,8 +109,9 @@ while fofo==1:
             print("3 | ",j,"| ",ä,"| ",öö,"|")
             print("  +---+---+---+")
             if form=="X":
-                print("Player 1 (X) won!")
+                print(bcolors.WARNING +"Player 1 (X) won!"+bcolors.ENDC)
                 pipi=1
+                sos=0
         elif k==p==üü=="O" or l==ü==ää=="O" or j==ä==öö=="O" or k==l==j=="O" or p==ü==ä=="O" or üü==ää==öö=="O" or k==ü==öö=="O" or j==ä==üü=="O":
             if form=="O":
                 print("    1   2   3  ")
@@ -120,11 +122,12 @@ while fofo==1:
                 print("  +---+---+---+")
                 print("3 | ",j,"| ",ä,"| ",öö,"|")
                 print("  +---+---+---+")
-                print("Player 2 (O) won!")
+                print(bcolors.WARNING +"Player 2 (O) won!"+bcolors.ENDC)
                 pipi=1
+                sos=0
             
 
-        if k and p and üü and l and ü and ää and j and ä and öö !="":
+        if k and p and üü and l and ü and ää and j and ä and öö !="" and sos==1:
             print("    1   2   3  ")
             print("  +---+---+---+")
             print("1 | ",k,"| ",p,"| ",üü,"|")
@@ -133,12 +136,12 @@ while fofo==1:
             print("  +---+---+---+")
             print("3 | ",j,"| ",ä,"| ",öö,"|")
             print("  +---+---+---+")
-            print("Game is over! Nobody won!")
+            print(bcolors.WARNING +"Game is over! Nobody won!"+bcolors.ENDC)
             pipi=1
         
     
         if pipi==1:
-            opa=input("Type 'r' to restart or 'e' to end: ")
+            opa=input(bcolors.WARNING +"Type 'r' to restart or 'e' to end: "+bcolors.ENDC)
             popo=1
             if opa=="r":
                 fofo=1
@@ -151,4 +154,7 @@ while fofo==1:
                 ö=0
                 pipi=0
 
-            
+        if x==2 and y==2 or y==1 or y==3 and k==l==j=="":
+            ln1=="" 
+            ln2==""
+            ln3=="" 
