07b3096ec20b8c2c54856c85ad0711164c9b08bb
Adrien <adrien.lucquiaud@gmail.com>
2021-08-26 10:42:24 +0200

    Bugfixes + Formatierungsänderung - Spiel ausgiebig getestes und weitere Fehler behoben, keine Fehler mehr zu sehen

--- TicTacToe.py
+++ TicTacToe.py
@@ -100,23 +100,31 @@ class TicTacToe:
         print("\t\t\t\t\t\t\t\t\t\t\t\t\t    Press 's' to start\n\n\n")
         start=input()
         if start==("s"):
-            print("Started the game")
+            print("\t\t\t\t\t\t\t\t\t\t\t\t\t     Started the game\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
     if start==("s"):
         while fofo==1:
-            print("    1   2   3  \n  +---+---+---+\n1 |   |   |   |\n  +---+---+---+\n2 |   |   |   |\n  +---+---+---+\n3 |   |   |   |\n  +---+---+---+")
+            #print("    1   2   3  \n  +---+---+---+\n1 |   |   |   |\n  +---+---+---+\n2 |   |   |   |\n  +---+---+---+\n3 |   |   |   |\n  +---+---+---+")
+            print("\t\t\t\t\t\t\t\t\t\t\t\t\t         1   2   3  ")
+            print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+            print("\t\t\t\t\t\t\t\t\t\t\t\t\t     1 |",k,"|",p,"|",üü,"|")
+            print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+            print("\t\t\t\t\t\t\t\t\t\t\t\t\t     2 |",l,"|",ü,"|",ää,"|")
+            print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+            print("\t\t\t\t\t\t\t\t\t\t\t\t\t     3 |",j,"|",ä,"|",öö,"|")
+            print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
 
             while ö==1:
                 if usa==1:
                 
-                    print("    1   2   3  ")
-                    print("  +---+---+---+")
-                    print("1 |",k,"|",p,"|",üü,"|")
-                    print("  +---+---+---+")
-                    print("2 |",l,"|",ü,"|",ää,"|")
-                    print("  +---+---+---+")
-                    print("3 |",j,"|",ä,"|",öö,"|")
-                    print("  +---+---+---+")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t         1   2   3  ")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     1 |",k,"|",p,"|",üü,"|")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     2 |",l,"|",ü,"|",ää,"|")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     3 |",j,"|",ä,"|",öö,"|")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
                 round+=1
                 if player==1:
                     form=asa="O"
@@ -125,9 +133,9 @@ class TicTacToe:
                     form=asa="X"
                     player-=1
 
-                print ("Spieler",player,"(",asa,")"" ist dran.")
-                x= int(input("Position x:"))
-                y= int(input("Position y:"))
+                print ("\n\t\t\t\t\t\t\t\t\t\t\t\t\tSpieler",player,"(",asa,")"" ist dran.")
+                x= int(input("\n\t\t\t\t\t\t\t\t\t\t\t\t\tPosition x:"))
+                y= int(input("\n\t\t\t\t\t\t\t\t\t\t\t\t\tPosition y:"))
                     
                 
                 if x==1 and y==2 and l==" ":
@@ -149,7 +157,7 @@ class TicTacToe:
                 elif x==3 and y==1 and üü==" ":
                     üü=form
                 elif x>3 or y>3:
-                    print(bcolors.WARNING +"Error - Falsche Eigabe! (Wähle zwischen 1-3)!"+bcolors.ENDC)
+                    print(bcolors.WARNING +"\t\t\t\t\t\t\t\t\t\t\t\t\tError - Falsche Eigabe! (Wähle zwischen 1-3)!"+bcolors.ENDC)
                     if player==1:
                         player+=1
                         form="O"
@@ -157,7 +165,7 @@ class TicTacToe:
                         player-=1
                         form="X"
                 else:
-                    print(bcolors.WARNING + "\tUnmöglicher Spielzug! Feld bereits besetzt!"+bcolors.ENDC)
+                    print(bcolors.WARNING + "\t\t\t\t\t\t\t\t\t\t\t\t\tUnmöglicher Spielzug! Feld bereits besetzt!"+bcolors.ENDC)
                     if player==1:
                         player+=1
                         form="O"
@@ -168,55 +176,64 @@ class TicTacToe:
 
                 if k==p==üü==["X","O"] or l==ü==ää=="X" or j==ä==öö=="X" or k==l==j=="X" or p==ü==ä=="X" or üü==ää==öö=="X" or k==ü==öö=="X" or j==ä==üü=="X":
                     
-                    print("    1   2   3  ")
-                    print("  +---+---+---+")
-                    print("1 |",k,"|",p,"|",üü,"|")
-                    print("  +---+---+---+")
-                    print("2 |",l,"|",ü,"|",ää,"|")
-                    print("  +---+---+---+")
-                    print("3 |",j,"|",ä,"|",öö,"|")
-                    print("  +---+---+---+")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t         1   2   3  ")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     1 |",k,"|",p,"|",üü,"|")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     2 |",l,"|",ü,"|",ää,"|")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     3 |",j,"|",ä,"|",öö,"|")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
                     if form=="X":
-                        print(bcolors.WARNING +"Player 1 (X) won!"+bcolors.ENDC)
+                        print(bcolors.WARNING +"\t\t\t\t\t\t\t\t\t\t\t\t\tPlayer 1 (X) won!"+bcolors.ENDC)
                         pipi=1
                         sos=0
                 elif k==p==üü=="O" or l==ü==ää=="O" or j==ä==öö=="O" or k==l==j=="O" or p==ü==ä=="O" or üü==ää==öö=="O" or k==ü==öö=="O" or j==ä==üü=="O":
                     if form=="O":
-                        print("    1   2   3  ")
-                        print("  +---+---+---+")
-                        print("1 |",k,"|",p,"|",üü,"|")
-                        print("  +---+---+---+")
-                        print("2 |",l,"|",ü,"|",ää,"|")
-                        print("  +---+---+---+")
-                        print("3 |",j,"|",ä,"|",öö,"|")
-                        print("  +---+---+---+")
-                        print(bcolors.WARNING +"Player 2 (O) won!"+bcolors.ENDC)
+                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t         1   2   3  ")
+                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t     1 |",k,"|",p,"|",üü,"|")
+                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t     2 |",l,"|",ü,"|",ää,"|")
+                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t     3 |",j,"|",ä,"|",öö,"|")
+                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                        print(bcolors.WARNING +"\t\t\t\t\t\t\t\t\t\t\t\t\tPlayer 2 (O) won!"+bcolors.ENDC)
                         pipi=1
                         sos=0
                     
 
                 if k!=" "  and p!=" "  and üü!=" "  and l!=" "  and ü!=" "  and ää!=" "  and j!=" "  and ä!=" "  and öö !=" " and sos==1:
-                    print("    1   2   3  ")
-                    print("  +---+---+---+")
-                    print("1 |",k,"|",p,"|",üü,"|")
-                    print("  +---+---+---+")
-                    print("2 |",l,"|",ü,"|",ää,"|")
-                    print("  +---+---+---+")
-                    print("3 |",j,"|",ä,"|",öö,"|")
-                    print("  +---+---+---+")
-                    print(bcolors.WARNING +"Game is over! Nobody won!"+bcolors.ENDC)
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t         1   2   3  ")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     1 |",k,"|",p,"|",üü,"|")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     2 |",l,"|",ü,"|",ää,"|")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     3 |",j,"|",ä,"|",öö,"|")
+                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                    print(bcolors.WARNING +"\t\t\t\t\t\t\t\t\t\t\t\t\tGame is over! Nobody won!"+bcolors.ENDC)
                     pipi=1
                 
             
                 if pipi==1:
-                    opa=input(bcolors.WARNING +"Type 'r' to restart or 'e' to end: "+bcolors.ENDC)
+                    opa=input(bcolors.WARNING +"\t\t\t\t\t\t\t\t\t\t\t\t\tType 'r' to restart or 'e' to end: "+bcolors.ENDC)
                     popo=1
                     if opa=="r":
                         fofo=1
                         usa=0
-                        print("    1   2   3  \n  +---+---+---+\n1 |   |   |   |\n  +---+---+---+\n2 |   |   |   |\n  +---+---+---+\n3 |   |   |   |\n  +---+---+---+")
+                        #print("    1   2   3  \n  +---+---+---+\n1 |   |   |   |\n  +---+---+---+\n2 |   |   |   |\n  +---+---+---+\n3 |   |   |   |\n  +---+---+---+")
                         k=p=üü=l=ü=ää=j=ä=öö=" "
                         pipi=0
+                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t         1   2   3  ")
+                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t     1 |",k,"|",p,"|",üü,"|")
+                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t     2 |",l,"|",ü,"|",ää,"|")
+                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t     3 |",j,"|",ä,"|",öö,"|")
+                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                        sos=1
                     else:
                         fofo=0
                         ö=0
