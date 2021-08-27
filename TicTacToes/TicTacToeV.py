6d7a620645747406ff8abf58d6e19e4684accc6e
Adrien <adrien.lucquiaud@gmail.com>
2021-08-27 09:54:20 +0200

    Code stark vereinfacht + schwerwiegender Fehler behoben (Definition des Gewinns bei X [X,O])

--- TicTacToes/TicTacToeV.py
+++ TicTacToes/TicTacToeV.py
@@ -10,10 +10,10 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 class TicTacToe:
-    player=ö=fofo=sos=1
+    player=fofo=1
     form=""
     k=l=j=p=ü=ä=üü=ää=öö=" "
-    usa=round=aktion_passiert=0   
+    round=aktion_passiert=0   
     text=1 #kann zwischen 1 und 6 sein (1 ist Favorit!)(danach: 6,(4),3,)
     if round==0:
         print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
@@ -99,138 +99,104 @@ class TicTacToe:
             print("\t\t\t\t\t\t\t\t\t\t\t\t\t     3 |",j,"|",ä,"|",öö,"|")
             print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
 
-            while ö==1:
-                if usa==1:
-                
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t         1   2   3  ")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     1 |",k,"|",p,"|",üü,"|")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     2 |",l,"|",ü,"|",ää,"|")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     3 |",j,"|",ä,"|",öö,"|")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                
+
+            
+            if player==1:
+                form=asa="O"
+                player+=1
+            else:
+                form=asa="X"
+                player-=1
+
+            print ("\n\t\t\t\t\t\t\t\t\t\t\t\t\tSpieler",player,"(",asa,")"" ist dran.")
+            x= int(input("\n\t\t\t\t\t\t\t\t\t\t\t\t\tPosition x:"))
+            y= int(input("\n\t\t\t\t\t\t\t\t\t\t\t\t\tPosition y:"))
+            print("\n\n\n\n")  
+            
+            if x==1 and y==2 and l==" ":
+                l=form
+            elif x==1 and y==1 and k==" ":
+                k=form
+            elif x==1 and y==3 and j==" ":
+                j=form
+            elif x==2 and y==1 and p==" ":
+                p=form
+            elif x==2 and y==3 and ä==" ":
+                ä=form
+            elif x==2 and y==2 and ü==" ":
+                ü=form
+            elif x==3 and y==3 and öö==" ":
+                öö=form
+            elif x==3 and y==2 and ää==" ":
+                ää=form
+            elif x==3 and y==1 and üü==" ":
+                üü=form
+            elif x>3 or y>3:
+                print(bcolors.WARNING +"\n\n\t\t\t\t\t\t\t\t\t\t\t\tError - Falsche Eigabe! (Wähle zwischen 1-3)!\n\n"+bcolors.ENDC)
                 if player==1:
-                    form=asa="O"
                     player+=1
+                    form="O"
                 else:
-                    form=asa="X"
                     player-=1
-
-                print ("\n\t\t\t\t\t\t\t\t\t\t\t\t\tSpieler",player,"(",asa,")"" ist dran.")
-                x= int(input("\n\t\t\t\t\t\t\t\t\t\t\t\t\tPosition x:"))
-                y= int(input("\n\t\t\t\t\t\t\t\t\t\t\t\t\tPosition y:"))
-                    
-                
-                if x==1 and y==2 and l==" ":
-                    l=form
-                elif x==1 and y==1 and k==" ":
-                    k=form
-                elif x==1 and y==3 and j==" ":
-                    j=form
-                elif x==2 and y==1 and p==" ":
-                    p=form
-                elif x==2 and y==3 and ä==" ":
-                    ä=form
-                elif x==2 and y==2 and ü==" ":
-                    ü=form
-                elif x==3 and y==3 and öö==" ":
-                    öö=form
-                elif x==3 and y==2 and ää==" ":
-                    ää=form
-                elif x==3 and y==1 and üü==" ":
-                    üü=form
-                elif x>3 or y>3:
-                    print(bcolors.WARNING +"\n\n\t\t\t\t\t\t\t\t\t\t\t\tError - Falsche Eigabe! (Wähle zwischen 1-3)!\n\n"+bcolors.ENDC)
-                    if player==1:
-                        player+=1
-                        form="O"
-                    else:
-                        player-=1
-                        form="X"
+                    form="X"
+            else:
+                print("\n\n\t\t\t\t\t\t\t\t\t\t\t\t"+bcolors.WARNING + "Unmöglicher Spielzug! Feld bereits besetzt!\n\n"+bcolors.ENDC)
+                if player==1:
+                    player+=1
+                    form="O"
                 else:
-                    print("\n\n\t\t\t\t\t\t\t\t\t\t\t\t"+bcolors.WARNING + "Unmöglicher Spielzug! Feld bereits besetzt!\n\n"+bcolors.ENDC)
-                    if player==1:
-                        player+=1
-                        form="O"
-                    else:
-                        player-=1
-                        form="X"
-                usa=1
-
-                if k==p==üü==["X","O"] or l==ü==ää=="X" or j==ä==öö=="X" or k==l==j=="X" or p==ü==ä=="X" or üü==ää==öö=="X" or k==ü==öö=="X" or j==ü==üü=="X":
-                    
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t         1   2   3  ")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     1 |",k,"|",p,"|",üü,"|")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     2 |",l,"|",ü,"|",ää,"|")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     3 |",j,"|",ä,"|",öö,"|")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                    if form=="X":
-                        print("\n\t\t\t\t\t\t\t\t\t\t\t\t\t      "+bcolors.WARNING+"Player 1 (X) won!\n"+bcolors.ENDC)
-                        aktion_passiert=1
-                        sos=0
-                elif k==p==üü=="O" or l==ü==ää=="O" or j==ä==öö=="O" or k==l==j=="O" or p==ü==ä=="O" or üü==ää==öö=="O" or k==ü==öö=="O" or j==ü==üü=="O":
-                    if form=="O":
-                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t         1   2   3  ")
-                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t     1 |",k,"|",p,"|",üü,"|")
-                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t     2 |",l,"|",ü,"|",ää,"|")
-                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t     3 |",j,"|",ä,"|",öö,"|")
-                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                        print("\n\t\t\t\t\t\t\t\t\t\t\t\t\t      "+bcolors.WARNING+"Player 2 (O) won!\n"+bcolors.ENDC)
-                        aktion_passiert=1
-                        sos=0
-                    
-
-                if k!=" "  and p!=" "  and üü!=" "  and l!=" "  and ü!=" "  and ää!=" "  and j!=" "  and ä!=" "  and öö !=" " and sos==1:
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t         1   2   3  ")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     1 |",k,"|",p,"|",üü,"|")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     2 |",l,"|",ü,"|",ää,"|")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t     3 |",j,"|",ä,"|",öö,"|")
-                    print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                    print("\n\t\t\t\t\t\t\t\t\t\t\t\t\t  "+bcolors.WARNING +"Game is over! Nobody won!\n\n"+bcolors.ENDC)
+                    player-=1
+                    form="X"
+            if k==p==üü=="X" or l==ü==ää=="X" or j==ä==öö=="X" or k==l==j=="X" or p==ü==ä=="X" or üü==ää==öö=="X" or k==ü==öö=="X" or j==ü==üü=="X" or k==p==üü=="O" or l==ü==ää=="O" or j==ä==öö=="O" or k==l==j=="O" or p==ü==ä=="O" or üü==ää==öö=="O" or k==ü==öö=="O" or j==ü==üü=="O":
+                
+                print("\t\t\t\t\t\t\t\t\t\t\t\t\t         1   2   3  ")
+                print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                print("\t\t\t\t\t\t\t\t\t\t\t\t\t     1 |",k,"|",p,"|",üü,"|")
+                print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                print("\t\t\t\t\t\t\t\t\t\t\t\t\t     2 |",l,"|",ü,"|",ää,"|")
+                print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                print("\t\t\t\t\t\t\t\t\t\t\t\t\t     3 |",j,"|",ä,"|",öö,"|")
+                print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                if form=="X":
+                    print("\n\t\t\t\t\t\t\t\t\t\t\t\t\t      "+bcolors.WARNING+"Player 1 (X) won!\n"+bcolors.ENDC)
+                    aktion_passiert=1
+                elif form=="O":
+                    print("\n\t\t\t\t\t\t\t\t\t\t\t\t\t      "+bcolors.WARNING+"Player 2 (O) won!\n"+bcolors.ENDC)
                     aktion_passiert=1
                 
+
+            if k!=" "  and p!=" "  and üü!=" "  and l!=" "  and ü!=" "  and ää!=" "  and j!=" "  and ä!=" "  and öö !=" " and aktion_passiert==0:
+                print("\t\t\t\t\t\t\t\t\t\t\t\t\t         1   2   3  ")
+                print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                print("\t\t\t\t\t\t\t\t\t\t\t\t\t     1 |",k,"|",p,"|",üü,"|")
+                print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                print("\t\t\t\t\t\t\t\t\t\t\t\t\t     2 |",l,"|",ü,"|",ää,"|")
+                print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                print("\t\t\t\t\t\t\t\t\t\t\t\t\t     3 |",j,"|",ä,"|",öö,"|")
+                print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
+                print("\n\t\t\t\t\t\t\t\t\t\t\t\t\t  "+bcolors.WARNING +"Game is over! Nobody won!\n\n"+bcolors.ENDC)
+                aktion_passiert=1
             
-                if aktion_passiert==1:
-                    opa=input("\t\t\t\t\t\t\t\t\t\t\t\t     "+bcolors.WARNING+"Type 'r' to restart or 'e' to end: "+bcolors.ENDC)
-                    popo=1
-                    if opa=="r":
-                        fofo=1
-                        usa=0
-                        k=p=üü=l=ü=ää=j=ä=öö=" "
-                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t         1   2   3  ")
-                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t     1 |",k,"|",p,"|",üü,"|")
-                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t     2 |",l,"|",ü,"|",ää,"|")
-                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t     3 |",j,"|",ä,"|",öö,"|")
-                        print("\t\t\t\t\t\t\t\t\t\t\t\t\t       +---+---+---+")
-                        sos=1
-                        aktion_passiert=0
-                    else:
-                        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-                        print("\t\t\t\t\t\t\t\t\t\t ████████╗██╗░░██╗░█████╗░███╗░░██╗██╗░░██╗░██████╗  ███████╗░█████╗░██████╗░")
-                        print("\t\t\t\t\t\t\t\t\t\t ╚══██╔══╝██║░░██║██╔══██╗████╗░██║██║░██╔╝██╔════╝  ██╔════╝██╔══██╗██╔══██╗")
-                        print("\t\t\t\t\t\t\t\t\t\t ░░░██║░░░███████║███████║██╔██╗██║█████═╝░╚█████╗░  █████╗░░██║░░██║██████╔╝")
-                        print("\t\t\t\t\t\t\t\t\t\t ░░░██║░░░██╔══██║██╔══██║██║╚████║██╔═██╗░░╚═══██╗  ██╔══╝░░██║░░██║██╔══██╗")
-                        print("\t\t\t\t\t\t\t\t\t\t ░░░██║░░░██║░░██║██║░░██║██║░╚███║██║░╚██╗██████╔╝  ██║░░░░░╚█████╔╝██║░░██║")
-                        print("\t\t\t\t\t\t\t\t\t\t ░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝╚═════╝░  ╚═╝░░░░░░╚════╝░╚═╝░░╚═╝\n")
-                        print("\t\t\t\t\t\t\t\t\t\t\t    ██████╗░██╗░░░░░░█████╗░██╗░░░██╗██╗███╗░░██╗░██████╗░")
-                        print("\t\t\t\t\t\t\t\t\t\t\t    ██╔══██╗██║░░░░░██╔══██╗╚██╗░██╔╝██║████╗░██║██╔════╝░")
-                        print("\t\t\t\t\t\t\t\t\t\t\t    ██████╔╝██║░░░░░███████║░╚████╔╝░██║██╔██╗██║██║░░██╗░")
-                        print("\t\t\t\t\t\t\t\t\t\t\t    ██╔═══╝░██║░░░░░██╔══██║░░╚██╔╝░░██║██║╚████║██║░░╚██╗")
-                        print("\t\t\t\t\t\t\t\t\t\t\t    ██║░░░░░███████╗██║░░██║░░░██║░░░██║██║░╚███║╚██████╔╝")
-                        print("\t\t\t\t\t\t\t\t\t\t\t    ╚═╝░░░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝╚═╝░░╚══╝░╚═════╝░\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-                        fofo=0
-                        ö=0
+        
+            if aktion_passiert==1:
+                opa=input("\t\t\t\t\t\t\t\t\t\t\t\t     "+bcolors.WARNING+"Type 'r' to restart or 'e' to end: "+bcolors.ENDC)
+                popo=1
+                if opa=="r":
+                    fofo=1
+                    k=p=üü=l=ü=ää=j=ä=öö=" "
+                    aktion_passiert=0
+                else:
+                    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    print("\t\t\t\t\t\t\t\t\t\t ████████╗██╗░░██╗░█████╗░███╗░░██╗██╗░░██╗░██████╗  ███████╗░█████╗░██████╗░")
+                    print("\t\t\t\t\t\t\t\t\t\t ╚══██╔══╝██║░░██║██╔══██╗████╗░██║██║░██╔╝██╔════╝  ██╔════╝██╔══██╗██╔══██╗")
+                    print("\t\t\t\t\t\t\t\t\t\t ░░░██║░░░███████║███████║██╔██╗██║█████═╝░╚█████╗░  █████╗░░██║░░██║██████╔╝")
+                    print("\t\t\t\t\t\t\t\t\t\t ░░░██║░░░██╔══██║██╔══██║██║╚████║██╔═██╗░░╚═══██╗  ██╔══╝░░██║░░██║██╔══██╗")
+                    print("\t\t\t\t\t\t\t\t\t\t ░░░██║░░░██║░░██║██║░░██║██║░╚███║██║░╚██╗██████╔╝  ██║░░░░░╚█████╔╝██║░░██║")
+                    print("\t\t\t\t\t\t\t\t\t\t ░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝╚═════╝░  ╚═╝░░░░░░╚════╝░╚═╝░░╚═╝\n")
+                    print("\t\t\t\t\t\t\t\t\t\t\t    ██████╗░██╗░░░░░░█████╗░██╗░░░██╗██╗███╗░░██╗░██████╗░")
+                    print("\t\t\t\t\t\t\t\t\t\t\t    ██╔══██╗██║░░░░░██╔══██╗╚██╗░██╔╝██║████╗░██║██╔════╝░")
+                    print("\t\t\t\t\t\t\t\t\t\t\t    ██████╔╝██║░░░░░███████║░╚████╔╝░██║██╔██╗██║██║░░██╗░")
+                    print("\t\t\t\t\t\t\t\t\t\t\t    ██╔═══╝░██║░░░░░██╔══██║░░╚██╔╝░░██║██║╚████║██║░░╚██╗")
+                    print("\t\t\t\t\t\t\t\t\t\t\t    ██║░░░░░███████╗██║░░██║░░░██║░░░██║██║░╚███║╚██████╔╝")
+                    print("\t\t\t\t\t\t\t\t\t\t\t    ╚═╝░░░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝╚═╝░░╚══╝░╚═════╝░\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+                    fofo=0
